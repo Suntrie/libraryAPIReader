d835db367eeb863505ad1e142d2fcdbc7d2bf847
Suntrie <is.egorova@mail.ru>
2018-11-27 10:42:07 +0300

    Добавлено сохранение информации о потерянных (например, за счёт отказа от опциональных, provided и system-зависимостей) и успешно загруженных классах.

---
+++ src/main/java/com/repoMiner/CustomClassLoader.java
@@ -0,0 +1,207 @@
+package com.repoMiner;
+
+import kotlin.Pair;
+import kotlin.Triple;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+
+public class CustomClassLoader extends ClassLoader {
+
+    private Set<JarFile> jarFiles=new HashSet<>();
+
+    private JarFile currentlyLoadingJar;
+
+    private Map<String, Class> loadedClasses = new HashMap<>();
+
+    private boolean parentClassLoaderAccess = false;
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+
+        String pointedFQN = getPointedFQN(name);
+
+        if (loadedClasses.keySet().contains(pointedFQN))
+            return loadedClasses.get(pointedFQN);
+
+        if (name.contains("java.") || parentClassLoaderAccess) {
+
+            loadedClasses.put(pointedFQN, super.loadClass(pointedFQN, true));
+
+            if (parentClassLoaderAccess)
+                parentClassLoaderAccess = false;
+
+            return loadedClasses.get(pointedFQN);
+        } else {
+
+            byte b[] = new byte[0];
+
+            String pathFQN = getPathFQN(name);
+
+            try {
+                b = fetchClassFromFS(pathFQN);
+                if (b == null) {
+                    parentClassLoaderAccess = true;
+                    return loadClass(pathFQN);
+                }
+            } catch (IOException e) {
+                throw new ClassNotFoundException("IOException");
+            }
+
+            Class definedClass = defineClass(pointedFQN, b, 0, b.length);
+            this.resolveClass(definedClass);
+
+            loadedClasses.put(pointedFQN,
+                    definedClass);
+
+            return definedClass;
+        }
+    }
+
+    @NotNull
+    private static String getPathFQN(String name) {
+        if ((name.contains(".")) && (!name.endsWith(".class"))) {
+            name = name.
+                    replaceAll("\\.", "/");
+            name = name.concat(".class");
+        }
+        return name;
+    }
+
+    @NotNull
+    private static String getPointedFQN(String name) {
+        String fullQualifiedClassName = name.
+                replaceAll("/", "\\.");
+
+        if (fullQualifiedClassName.endsWith(".class"))
+            fullQualifiedClassName = fullQualifiedClassName.
+                    substring(0, name.lastIndexOf("."));
+        return fullQualifiedClassName;
+    }
+
+    private byte[] fetchClassFromFS(String name) throws IOException {
+
+        if (currentlyLoadingJar.getJarEntry(name) == null)
+            return null;
+
+        InputStream jarInputStream = currentlyLoadingJar
+                .getInputStream(currentlyLoadingJar.getJarEntry(name));
+
+        byte[] bytes = getBytes(jarInputStream);
+
+        jarInputStream.close();
+        return bytes;
+
+    }
+
+    private static byte[] getBytes(InputStream is) throws IOException {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream();) {
+            byte[] buffer = new byte[0xFFFF];
+            for (int len; (len = is.read(buffer)) != -1; )
+                os.write(buffer, 0, len);
+            os.flush();
+            return os.toByteArray();
+        }
+    }
+
+    private static JarEntry getNextJarEntryMatches(Enumeration<JarEntry> jarEntryEnumeration,
+                                                   String template) {
+        JarEntry jarEntry = null;
+
+        while (true) {
+
+            if (!jarEntryEnumeration.hasMoreElements()) {
+
+                jarEntry = null;
+                break;
+            }
+            jarEntry = jarEntryEnumeration.nextElement();
+            if ((jarEntry == null) || (jarEntry.getName().endsWith(template))) {
+                break;
+            }
+        }
+
+        return jarEntry;
+    }
+
+
+    /*@jarPath - path to base libraries dir
+     * */
+    // first - set of loaded classes
+    // second - set of missed classes
+    public Pair<Set<Class>, Set<String>> loadLibraryClassSet(String jarPath)
+            throws IOException {
+
+        Set<Class> loadedClasses = new HashSet<Class>();
+        Set<String> missedClassNames = new HashSet<String>();
+
+        currentlyLoadingJar = new JarFile(jarPath);
+        JarEntry jarEntry;
+
+        Enumeration<JarEntry> jarEntryEnumeration = currentlyLoadingJar.entries();
+
+        while (true) {
+
+            jarEntry = getNextJarEntryMatches(jarEntryEnumeration, ".class");
+
+            if (jarEntry == null) {
+                break;
+            }
+
+            Class clazz = null;
+
+            try {
+                clazz = this.
+                        loadClass(jarEntry.getName());  //sic - анонимные классы и вложенные интерфейсы будут давать null
+            }catch (ClassNotFoundException | NoClassDefFoundError e){ // NoClassDefFoundError - класс был доступен при компиляции, но не доступен в runtime
+                missedClassNames.add(jarEntry.getName());             // например, потому что мы не грузим чужие опциональные зависимости
+            }
+
+            if (clazz!=null)
+                loadedClasses.add(clazz);
+        }
+
+        jarFiles.add(currentlyLoadingJar);
+
+        return new Pair<>(loadedClasses, missedClassNames);
+    }
+
+    public Set<String> getMissedClassNames(String jarPath) throws IOException {
+        return loadLibraryClassSet(jarPath).getSecond();
+    }
+
+    public Triple<Set<Method>, Set<Class>, Set<Class>> getExecutableLibraryMethods(String jarPath)
+            throws IOException {
+
+        Set<Method> executableMethods = new HashSet<Method>();
+
+
+        Set<Class> definedClasses=new HashSet<>();
+        Set<Class> missedMethodDef=new HashSet<>();
+
+        for (Class libraryAPIClass : loadLibraryClassSet(jarPath).getFirst()) {
+            if (!Modifier.isAbstract(libraryAPIClass.getModifiers())) {
+                try {
+                    executableMethods.addAll(Arrays.stream(libraryAPIClass.getMethods())
+                            .filter(it -> Modifier.isPublic(it.getModifiers())).collect(Collectors.toSet()));
+                    definedClasses.add(libraryAPIClass);
+                }catch (NoClassDefFoundError e){
+                    missedMethodDef.add(libraryAPIClass);
+                }
+            }
+        }
+
+        //executableMethods.stream().forEach(it-> System.out.println(it.getName()));
+
+        return new Triple<>(executableMethods, definedClasses, missedMethodDef);
+    }
+
+}
--- src/main/kotlin/com/repoMiner/Hello.kt
+++ src/main/kotlin/com/repoMiner/Hello.kt
@@ -1,18 +1,22 @@
 package com.repoMiner
 
-import com.repoMiner.PackageUtils.getExecutableLibraryMethods
 import java.net.URL
 import java.util.*
 
 fun main(args: Array<String>) {
+
 
     val jarURLs = ArrayList<URL>()
 
     jarURLs.add(URL("file:///"+
             /*"/E:/testLib/out/artifacts/testLib_jar/testLib.jar"*/"/C:/Users/Neverland/.m2/repository/com/netflix/spectator/spectator-api/0.57.1/spectator-api-0.57.1.jar"))
 
+
+    val customClassLoader=CustomClassLoader();
+
     for (jarURL in jarURLs) {
-        getExecutableLibraryMethods(jarURL.getPath())
+        customClassLoader.loadLibraryClassSet(jarURL.path)
+        customClassLoader.getExecutableLibraryMethods(jarURL.getPath())
     }
 
 
